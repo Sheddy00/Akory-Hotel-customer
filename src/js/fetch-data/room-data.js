32bb6c3fdf083004200c620fbb1c7eae7afe845b
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2023-08-06 12:33:46 +0300

    feat : "room fetch plus"

--- src/js/fetch-data/room-data.js
+++ src/js/fetch-data/room-data.js
@@ -1,21 +1,41 @@
 function afficherDonnees(data) {
     const roomListDiv = document.querySelector(".room_conainer");
-    // Parcourir les données JSON et les ajouter à l'interface
+
     data.forEach(room => {
+        const imagePath = room.images_paths ?
+            `./../../../../../../../../..${room.images_paths.replace(/\s/g, '\\ ')}` :
+            "../../public/assets/images/alt_image.png";
+        
+        const promotionsDayValue = room.promotions_day
+         ? parseFloat(room.promotions_day).toFixed(2) : null;
+
         const roomDiv = document.createElement("div");
         roomDiv.innerHTML = `
-        <div class="room_items">
-            <div class="room_image_content"
-                style="background-image: url(../../public/assets/images/room/room-1.jpg);">
+            <div class="room_items">
+                <div class="room_image_content"  "
+                style="background-image: url(${imagePath});"> 
                 ${room.number}
             </div>
+
             <div class="room_details_content">
                 <div class="name_rate">
                     <p id="room_type_name">${room.room_type}</p>
                     <div class="room_rate">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
+                    ${
+                        room.room_type === "solo"
+                            ? `<i class="fa-solid fa-star star_color"></i>
+                               <i class="fa-solid fa-star"></i>
+                               <i class="fa-solid fa-star"></i>`
+                            : room.room_type === "twin" || room.room_type === "family"
+                            ? `<i class="fa-solid fa-star star_color"></i>
+                               <i class="fa-solid fa-star star_color"></i>
+                               <i class="fa-solid fa-star"></i>`
+                            : room.room_type === "VIP"
+                            ? `<i class="fa-solid fa-star star_color"></i>
+                               <i class="fa-solid fa-star star_color"></i>
+                               <i class="fa-solid fa-star star_color"></i>`
+                            : ''
+                    }
                     </div>
                 </div>
 
@@ -25,20 +45,24 @@ function afficherDonnees(data) {
                     <span>${room.hotel} </span> <span id="location"> Antananarivo</span>
                 </div>
                 </p>
-                <div class="room_description_with_prommotion">
-                    This is your chance to take advantage, as the price is reduced by
-                    <span id="netPrice"> ${room.promotions_day}Ar/nigth</span>
-                    until the end of promotion
-                    <span id="promotion_name"><a href="../../index.html"> Sarika</a></span> on
-                    <span id="promotion_end"> 06 janvier 2024</span> .
-                    While the net price is:
-                    <span id="net_price"> ${room.price}/night</span>
-                </div>
-                <div class="room_description_without_prommotion">
-                    Even if there are no promotions during this
-                    period, the price remains affordable:
-                    <span id="net_price"> 2500Ar/night</span>
-                </div>
+                ${
+                    room.promotions_day ?
+                    ` <div class="room_description_with_prommotion">
+                        This is your chance to take advantage, as the price is reduced by
+                        <span id="netPrice"> ${promotionsDayValue}Ar/nigth</span>
+                        until the end of promotion
+                        <span id="promotion_name"><a href="../../index.html"> Sarika</a></span> on
+                        <span id="promotion_end"> 06 janvier 2024</span> .
+                        While the net price is:
+                        <span id="net_price"> ${room.price}/night</span>
+                    </div> `
+                    :
+                ` <div class="room_description_without_prommotion">
+                        Even if there are no promotions during this
+                        period, the price remains affordable:
+                        <span id="net_price"> ${room.price}/night</span>
+                </div>`
+                }
             </div>
             <div class="room_booking_content">
                 <p id="booking_now">BOOKING NOW: </p>
@@ -46,13 +70,13 @@ function afficherDonnees(data) {
             </div>
             <div class="room_features_price">
                 <div class="features_contain">
-                ${room.sea_view ? `<i id="sea_view" class="fa-solid fa-water"></i>`:''}
-                ${room.vip_category ? `<span id="vip_category">VIP</span>`:''}
-                ${room.hot_water ? `<i id="hot_water" class="fa-solid fa-hot-tub-person"></i>`:''}
-                ${room.wifi_available ? `<i id="wifi_available" class="fa-solid fa-wifi"></i>`:''}
-                ${room.room_service ? `<i id="room_service" class="fa-solid fa-bell-concierge"></i>`:''}
-                ${room.mini_bar ? `  <i id="mini_bar" class="fa-solid fa-champagne-glasses"></i>`:''}
-                ${room.flat_screen ? `<i id="flat_screen" class="fa-solid fa-tv"></i>`:''}
+                ${room.sea_view ? `<i id="sea_view" class="fa-solid fa-water"></i>` : ''}
+                ${room.vip_category ? `<span id="vip_category">VIP</span>` : ''}
+                ${room.hot_water ? `<i id="hot_water" class="fa-solid fa-hot-tub-person"></i>` : ''}
+                ${room.wifi_available ? `<i id="wifi_available" class="fa-solid fa-wifi"></i>` : ''}
+                ${room.room_service ? `<i id="room_service" class="fa-solid fa-bell-concierge"></i>` : ''}
+                ${room.mini_bar ? `  <i id="mini_bar" class="fa-solid fa-champagne-glasses"></i>` : ''}
+                ${room.flat_screen ? `<i id="flat_screen" class="fa-solid fa-tv"></i>` : ''}
                 </div>
             </div>
     </div>
@@ -62,12 +86,11 @@ function afficherDonnees(data) {
 
 }
 
-
-function getRoomsListNetAndGrosPrice(){
+function getRoomsListNetAndGrosPrice() {
     fetch("http://localhost:8000/RoomsListNetAndGrosPrice")
-    .then(response => response.json())
-    .then(data => afficherDonnees(data))
-    .catch(error => console.error("Une erreur s'est produite :", error));
+        .then(response => response.json())
+        .then(data => afficherDonnees(data))
+        .catch(error => console.error("Une erreur s'est produite :", error));
 }
 
 getRoomsListNetAndGrosPrice();
