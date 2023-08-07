22b8e819e1231ed11dd4faa69cc82a8c89f61253
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2023-08-07 17:30:33 +0300

    feat: "hover features"

--- src/js/room.js
+++ src/js/room.js
@@ -6,17 +6,26 @@ bookingViaRoom.forEach(el => {
     });
 });
 
+// recuperation features:
+
 // const roomFeatureIcons = document.querySelectorAll(".features_icon");
+// const tableValuesFeatures = [];
+
 // roomFeatureIcons.forEach(icon => {
-//     icon.addEventListener("click", (ev) => {
-//         console.log(icon.getAttribute("data-active"))
-//         ev.stopPropagation();
-//         if (icon.getAttribute("data-active") === false) {
-//             icon.classList.add('star_color');
-//             icon.setAttribute("data-active", "true");
-//         } else {
-//             icon.classList.remove('star_color');
-//             icon.setAttribute("data-active", "false");
-//         }
-//     });
+//   icon.addEventListener("click", () => {
+//     const isActive = icon.getAttribute("data-active");
+//     const index = Array.from(roomFeatureIcons).indexOf(icon);
+//     const newValue = isActive === "false" ? "true" : "false";
+
+//     icon.setAttribute("data-active", newValue);
+//     tableValuesFeatures[index] = newValue;
+
+//     if (newValue === "true") {
+//       icon.classList.add("star_color");
+//     } else {
+//       icon.classList.remove("star_color");
+//     }
+
+//     console.log(tableValuesFeatures);
+//   });
 // });
